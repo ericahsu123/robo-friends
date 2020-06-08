ac858cd1b03199f5cd6b5c2c9e2c56a7133023f6
許靜寧 <Erica@xujingnings-MacBook.local>
2020-06-08 21:54:31 +0800

    Updates

---
+++ precache-manifest.47cd9e4b3433d7321d14105c93eb3956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b999208784147e4d8d4d6d01b9022e",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "f5aeeb2244e46aefcfbe",
+    "url": "/robo-friends/static/css/main.d1eff3fb.chunk.css"
+  },
+  {
+    "revision": "d93ed0f26752f4248338",
+    "url": "/robo-friends/static/js/2.e4a4c9f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.e4a4c9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5aeeb2244e46aefcfbe",
+    "url": "/robo-friends/static/js/main.770805d3.chunk.js"
+  },
+  {
+    "revision": "3f75afefe0792e109c28",
+    "url": "/robo-friends/static/js/runtime-main.b414033a.js"
+  }
+]);
